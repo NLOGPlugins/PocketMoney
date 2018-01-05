3ce4d2bec463bf4c8831bf429246a64ee4408155
NLOG <sorisem4106@naver.com>
2018-01-05 12:04:36 +0900

    Update RankSubCommand.php

--- src/PocketMoney/command/subcommand/RankSubCommand.php
+++ src/PocketMoney/command/subcommand/RankSubCommand.php
@@ -13,13 +13,14 @@ class RankSubCommand extends SubCommand {
 
     public function execute(CommandSender $sender, array $args): bool {
         //TODO: see rank
-        $page = isset($args[0]) ? (int)$args[0] : 1;
+        $page = isset($args[0]) ? (int) $args[0] : 1;
         $max = count($this->getPlugin()->getServer()->getNameBans()->getEntries());
         $message = str_ireplace(["{PAGE}", "{MAX}"], [$page, $this->getPlugin()->getRankMaxPage($max)], $this->getPlugin()->getMessage("rank-prefix"));
         $rank = $this->getPlugin()->getRankPage($page);
         if ($rank === null) return false;
-        foreach ($rank as $rank => $arr)
-            $message .= "\n".str_ireplace(["{RANK}", "{PLAYER}", "{MONEY}"], [$rank, $arr[0], $arr[1]], $this->getPlugin()->getMessage("rank-format"));
+        foreach ($rank as $rank => $name) {
+            $message .= "\n".str_ireplace(["{RANK}", "{PLAYER}", "{MONEY}"], [$rank, $name, $this->getPlugin()->getMoney($name)], $this->getPlugin()->getMessage("rank-format"));
+		}
         $sender->sendMessage($message);
         return true;
     }
